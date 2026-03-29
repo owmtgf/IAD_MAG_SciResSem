dae83c9a208ec46b42b4a6b0e389b45404a6c43f
melpomene310 <kleeeeey503@gmail.com>
2026-03-29 19:30:34 +0300

    add stochastic programming

--- LAB1/run_experiments.py
+++ LAB1/run_experiments.py
@@ -1,12 +1,12 @@
-from instance_generator import InstanceGenerator
+from instance_generator import RobustInstanceGenerator, StochasticInstanceGenerator
 from solvers import RobustAssignment, StochasticAssignment
 
 
 if __name__ == "__main__":
 
     # robust
-    gen = InstanceGenerator(seed=42)
-    instances = gen.generate_robust_batch(n=10, num_instances=100)
+    gen = RobustInstanceGenerator(seed=42)
+    instances = gen.generate_batch(n=10, num_instances=100)
 
     for c_bar, d in instances:
         model = RobustAssignment(c_bar, d, Gamma=3)
@@ -14,12 +14,14 @@ if __name__ == "__main__":
         print(obj)
 
     # stochastic
-    gen = InstanceGenerator(seed=42)
-    instances = gen.generate_stochastic_batch(n=10, num_instances=100, k=30)
+    gen = StochasticInstanceGenerator(seed=42)
+    instances = gen.generate_batch(n=10, num_instances=100, k=30)
 
-    for scenarios in instances:
-        # scenarios = [c^1, c^2, ..., c^30]
-        model = StochasticAssignment()
-        sol, obj = model.solve()
+    for cost_matrix in instances:
+        model = StochasticAssignment(cost_matrix)
+        print("RN SAA solution:")
+        sol, obj = model.solve_risk_neutral()
         print(obj)
-        
+        print("RA SAA solution:")
+        sol, obj = model.solve_risk_averse()
+        print(obj)
